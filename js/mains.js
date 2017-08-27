c30d4de38d01cee20b3ff73a3ecb9fecbf804b68
IwonaJarkiewicz <politea@gmail.com>
2017-08-27 10:13:38 +0200

    wyświetlanie danych

--- js/mains.js
+++ js/mains.js
@@ -4,11 +4,29 @@ $(function () {
     $.ajax({
         url: 'http://echo.jsontest.com/userId/108/userName/Akademia108/userURL/akademia108.pl',
         dataType: 'json',
-        success: function (resultJson) {
-            console.log(resultJson);
+        success: function (resultJSON) {
+            console.log(resultJSON);
+
+            var pUserId = document.createElement('p');
+            var pUserName = document.createElement('p');
+            var pUserUrl = document.createElement('p');
+
+            pUserId.innerHTML = 'User ID: ' + resultJSON.userId;
+            pUserId.innerHTML = 'User name: ' + resultJSON.userName;
+            pUserId.innerHTML = 'User url http://' + resultJSON.userURL;
+
+            document.body.appendChild(pUserId);
+            document.body.appendChild(pUserName);
+            document.body.appendChild(pUserUrl);
+
+
         },
         oneerror: function (msg) {
             console.log(msg);
         }
     });
+
+    /* $getJSON('http://echo.jsontest.com/userId/108/userName/Akademia108/userURL/akademia108.pl', function (data) {
+         console.log(data);
+     });*/
 });
